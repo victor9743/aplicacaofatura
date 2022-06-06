e823d94ee1aeb21c642c9e89bce588279c523a0a
victor costa <victorcosta9743@gmail.com>
2022-06-06 12:44:30 -0300

    tentativa de cooreção erro 404

--- config/database.php
+++ config/database.php
@@ -122,7 +122,12 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'predis'),
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
 
         'default' => [
             'url' => env('REDIS_URL'),
